d2cb7e3b50b073feaeca3ffbdfb9f359fc3692c5
root <root@localhost.localdomain>
2022-11-02 00:47:55 +0800

    完成lab01 20301038 何毅

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,15 +1,25 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 use core::arch::asm;
+use core::panic::PanicInfo;
+use core::fmt::{self, Write};
 
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -29,7 +39,32 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[no_mangle]
 extern "C" fn _start() {
+    //loop{};
+    println!("Hello, world!");
     sys_exit(9);
 }
