c6e3ee7c4c47ab8d7665c6f79ed4d87f4a4006d1
root <root@localhost.localdomain>
2022-11-08 11:46:15 +0800

    完成lab02 第五部分 20301038 何毅

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,27 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod sbi;
+mod console;
+
 use core::arch::asm;
 use core::panic::PanicInfo;
-use core::fmt::{self, Write};
 use core::arch::global_asm;
 
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_WRITE: usize = 64;
 global_asm!(include_str!("entry.asm"));
 
-struct Stdout;
 
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap();
-}
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -54,19 +45,7 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
   syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-    }
-}
 
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-    }
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
