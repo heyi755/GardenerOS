02ff1ccc890c6bd38baf844650d17824dc2ff486
root <root@localhost.localdomain>
2022-12-12 02:41:13 +0800

    代码不小心搞崩了，继续修以前的bug，呜呜 20301038-何毅

--- os/src/task/context.rs
+++ os/src/task/context.rs
@@ -1,24 +1,14 @@
-use crate::trap::trap_return;
-
 #[repr(C)]
 pub struct TaskContext {
     ra: usize,
-    sp: usize,
     s: [usize; 12],
 }
 
 impl TaskContext {
-    pub fn zero_init() -> Self {
+    pub fn goto_restore() -> Self {
+        extern "C" { fn __restore(); }
         Self {
-            ra: 0,
-            sp: 0,
-            s: [0; 12],
-        }
-    }
-    pub fn goto_trap_return(kstack_ptr: usize) -> Self {
-        Self {
-            ra: trap_return as usize,
-            sp: kstack_ptr,
+            ra: __restore as usize,
             s: [0; 12],
         }
     }
